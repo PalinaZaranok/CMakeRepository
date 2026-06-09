5880df888c5c11e14182d7184d8200d9cdaeb209
polina_zarenok_351003 <palina.zaranok@gmail.com>
2026-06-09 19:56:01 +0300

    build: Собран проект с помощью CMake

---
+++ CMakeProject/CMakeProject.cpp
@@ -0,0 +1,17 @@
+﻿// CMakeProject.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Enter numbers" << endl;
+	int a, b, c;
+	cin >> a ;
+	cin >> b;
+	c = a + b;
+	cout << c << endl;
+	return 0;
+}
